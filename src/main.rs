aa2ae1a1cdb75c787904b687743aa95c7bf82605
Colepng <gamingwithcole7@gmail.com>
2023-03-14 11:59:37 -0400

    Added a clear button

--- src/main.rs
+++ src/main.rs
@@ -50,55 +50,60 @@ impl eframe::App for MyApp<'_> {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Calulator");
 
-            if self.mode_int {
-                ui.horizontal(|ui| {
-                    if ui.button("7").clicked() {
-                        self.input.push('7');
-                    } else if ui.button("8").clicked() {
-                        self.input.push('8')
-                    } else if ui.button("9").clicked() {
-                        self.input.push('9');
-                    } else if ui.button("*").clicked() && !check_if_operator(&self.input) {
-                        self.input.push('*');
-                    }
-                });
+            ui.horizontal(|ui| {
+                if ui.button("AC").clicked() {
+                    self.input = "".to_owned();
+                }
+            });
 
-                ui.horizontal(|ui| {
-                    if ui.button("4").clicked() {
-                        self.input.push('4');
-                    } else if ui.button("5").clicked() {
-                        self.input.push('5');
-                    } else if ui.button("6").clicked() {
-                        self.input.push('6');
-                    } else if ui.button("-").clicked() && !check_if_operator(&self.input) {
-                        self.input.push('-');
-                    }
-                });
+            ui.horizontal(|ui| {
+                if ui.button("7").clicked() {
+                    self.input.push('7');
+                } else if ui.button("8").clicked() {
+                    self.input.push('8')
+                } else if ui.button("9").clicked() {
+                    self.input.push('9');
+                } else if ui.button("*").clicked() && !check_if_operator(&self.input) {
+                    self.input.push('*');
+                }
+            });
 
-                ui.horizontal(|ui| {
-                    if ui.button("1").clicked() {
-                        self.input.push('1');
-                    } else if ui.button("2").clicked() {
-                        self.input.push('2')
-                    } else if ui.button("3").clicked() {
-                        self.input.push('3')
-                    } else if ui.button("+").clicked() && !check_if_operator(&self.input) {
-                        self.input.push('+');
-                    }
-                });
+            ui.horizontal(|ui| {
+                if ui.button("4").clicked() {
+                    self.input.push('4');
+                } else if ui.button("5").clicked() {
+                    self.input.push('5');
+                } else if ui.button("6").clicked() {
+                    self.input.push('6');
+                } else if ui.button("-").clicked() && !check_if_operator(&self.input) {
+                    self.input.push('-');
+                }
+            });
 
-                ui.horizontal(|ui| {
-                    if ui.button("0").clicked() {
-                        self.input.push('0');
-                    } else if ui.button(".").clicked() {
-                        self.input.push('.');
-                    } else if ui.button("=").clicked() {
-                        calulate(&self.input);
-                    } else if ui.button("/").clicked() && !check_if_operator(&self.input) {
-                            self.input.push('/');
-                    }
-                });
-            }
+            ui.horizontal(|ui| {
+                if ui.button("1").clicked() {
+                    self.input.push('1');
+                } else if ui.button("2").clicked() {
+                    self.input.push('2')
+                } else if ui.button("3").clicked() {
+                    self.input.push('3')
+                } else if ui.button("+").clicked() && !check_if_operator(&self.input) {
+                    self.input.push('+');
+                }
+            });
+
+            ui.horizontal(|ui| {
+                if ui.button("0").clicked() {
+                    self.input.push('0');
+                } else if ui.button(".").clicked() {
+                    self.input.push('.');
+                } else if ui.button("=").clicked() {
+                    self.input.push('=');
+                    calulate(&self.input);
+                } else if ui.button("/").clicked() && !check_if_operator(&self.input) {
+                        self.input.push('/');
+                }
+            });
 
             self.display = &self.input;
             ui.heading(self.display);
